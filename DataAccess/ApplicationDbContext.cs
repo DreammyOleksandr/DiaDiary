d1840935757a8c33f62e850a97fdf71e1aec1c6d
Alex Bondarenko <albom2004q@gmail.com>
2023-03-05 20:40:18 +0200

    Removing code duplicating in DbManager class

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -5,8 +5,9 @@ namespace DataAccess;
 public class ApplicationDbContext
 {
     private const string ConnectionString = "mongodb://127.0.0.1:27017";
-    protected const string DbName = "diabeticslogs";
-    protected static readonly string collectionName = "LogEntries";
+    private const string DbName = "diabeticslogs";
+    protected const string collectionName = "LogEntries";
     
-    protected static readonly MongoClient _client = new MongoClient(ConnectionString);
+    private static readonly MongoClient _client = new MongoClient(ConnectionString);
+    protected static IMongoDatabase _db = _client.GetDatabase(DbName);
 }
--- DataAccess/MongoDbManager.cs
+++ DataAccess/MongoDbManager.cs
@@ -4,19 +4,16 @@ using MongoDB.Driver;
 namespace DataAccess;
 
 public class MongoDbManager : ApplicationDbContext
-{
-
+{ 
+    static IMongoCollection<LogEntry> collection = _db.GetCollection<LogEntry>(collectionName);
+    
     public static async Task Create(LogEntry logEntry)
     {
-        var _db = _client.GetDatabase(DbName);
-        var collection = _db.GetCollection<LogEntry>(collectionName);
         await collection.InsertOneAsync(logEntry);
     }
     //Read
     public static async Task GetAll()
     {
-        var _db = _client.GetDatabase(DbName);
-        var collection = _db.GetCollection<LogEntry>(collectionName);
         var entries = await collection.FindAsync(_ => true);
 
         foreach (var entry in entries.ToList())
@@ -27,16 +24,14 @@ public class MongoDbManager : ApplicationDbContext
     //Update
     public static async Task Update()
     {
-        var _db = _client.GetDatabase(DbName);
-        var collection = _db.GetCollection<LogEntry>(collectionName);
+
     }
     //Delete
     public static async Task DeleteByGlucoseLevel()
     {
         Console.WriteLine("Choose note which you want to delete by glucose level");
         string GlucoseEntrieToDelete = Console.ReadLine();
-        var _db = _client.GetDatabase(DbName);
-        var collection = _db.GetCollection<LogEntry>(collectionName);
+        
         collection.DeleteOneAsync(p => p.GlucoseLevel.ToString() == GlucoseEntrieToDelete);
     }
 }
