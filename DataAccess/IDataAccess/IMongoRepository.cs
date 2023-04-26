25e59379bf218cb4c1c175bc9a26dc140b2dda33
Alex Bondarenko <albom2004q@gmail.com>
2023-04-26 23:54:07 +0300

    Changing MongoRepository.cs so it can work w generics

--- DataAccess/IDataAccess/IMongoRepository.cs
+++ DataAccess/IDataAccess/IMongoRepository.cs
@@ -3,7 +3,7 @@ namespace DataAccess.IDataAccess;
 public interface IMongoRepository<T>
 {
       async Task Create(){}
-      void GetAll(){}
+      IEnumerable<T> GetAll();
       async Task Update(){}
       async Task Delete(){}
       async Task DeleteAll(){}
--- DataAccess/MongoRepository.cs
+++ DataAccess/MongoRepository.cs
@@ -1,4 +1,5 @@
-﻿using Models;
+﻿using System.Linq.Expressions;
+using Models;
 using MongoDB.Driver;
 using DataAccess.IDataAccess;
 using MongoDB.Bson;
@@ -8,74 +9,43 @@ namespace DataAccess;
 
 public class MongoRepository<T> : IMongoRepository<T> where T : class
 {
-    private static IMongoCollection<LogEntry> _collection;
-    
+    private readonly string _collectionName = "LogEntries";
+    private static IMongoCollection<T> _collection;
+
     public MongoRepository(IMongoDatabase database)
     {
-        _collection = database.GetCollection<LogEntry>("LogEntries");
+        _collection = database.GetCollection<T>(_collectionName);
     }
-    
+
     //Create
-    public async Task Create()
+    public async Task Create(T entity)
     {
-        LogEntry logEntry = new LogEntry();
-        
-        Console.Write("Glucose level:");
-        logEntry.GlucoseLevel = double.Parse(Console.ReadLine());
-        Console.Write("Short term insulin injected:");
-        logEntry.ShortTermInsulin = byte.Parse(Console.ReadLine());
-        Console.Write("Long term insulin injected:");
-        logEntry.LongTermInsulin = byte.Parse(Console.ReadLine());
-        Console.Write("Carbs eaten:");
-        logEntry.CarbsInBreadUnits = double.Parse(Console.ReadLine());
-        Console.WriteLine("Notes:");
-        logEntry.Notes = Console.ReadLine() ?? "*There was no notes written*";
-        await _collection.InsertOneAsync(logEntry);
+        await _collection.InsertOneAsync(entity);
     }
+
     //Read
-    public void GetAll()
+    public IEnumerable<T> GetAll()
     {
-        var entries = _collection.Find(_ => true);
-
-        foreach (var entry in entries.ToList())
-        {
-            Console.WriteLine($"Time: {entry.Time:t}\n\n"+
-                              $"Glucose level: {entry.GlucoseLevel}\n" +
-                              $"Short term insulin: {entry.ShortTermInsulin}\n" +
-                              $"Long term insulin: {entry.LongTermInsulin}\n" +
-                              $"Carbs (Bread units): {entry.CarbsInBreadUnits}\n" +
-                              $"{entry.Notes}\n\n");
-        }
+        return _collection.Find(_ => true).ToList();
     }
+
     //Update
-    public async Task Update()
+    public async Task FirstOrDefault(Expression<Func<T, bool>> filter, T entity)
     {
-        Console.WriteLine("Enter Glucose level of log you want to Update");
-        double glucoseToFind = double.Parse(Console.ReadLine());
-        var filter = new BsonDocument("GlucoseLevel", glucoseToFind);
-        
-        Console.WriteLine("Enter new Glucose level");
-        double updatedValue = double.Parse(Console.ReadLine());
-        var updated = new BsonDocument("$set", new BsonDocument("GlucoseLevel", updatedValue));
-
-        var result = await _collection.UpdateOneAsync(filter, updated);
+        await _collection.ReplaceOneAsync(filter, entity);
     }
+
     //Delete
-    public static async Task Delete()
+    public static async Task Delete(Expression<Func<T, bool>> filter)
     {
-        Console.WriteLine("Choose note which you want to delete by glucose level");
-        string GlucoseEntrieToDelete = Console.ReadLine();
-        
-        var filter = Builders<LogEntry>.Filter.Eq("GlucoseLevel", GlucoseEntrieToDelete);
         await _collection.DeleteOneAsync(filter);
     }
 
     public static async Task DeleteAll()
     {
-        
         Console.WriteLine("This option will delete all your entries\n" +
                           "Hit enter to continue/backspace to discard changes");
-        
+
         ConsoleKeyInfo keyInfo = Console.ReadKey(true);
         ConsoleKey keyPressed = keyInfo.Key;
 
@@ -86,7 +56,12 @@ public class MongoRepository<T> : IMongoRepository<T> where T : class
 
         if (keyPressed == ConsoleKey.Backspace)
         {
-            
+            return;
         }
+    }
+    
+    public void Insert(T entity)
+    {
+        _collection.InsertOne(entity);
     }
 }
