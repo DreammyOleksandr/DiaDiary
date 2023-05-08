ff5612e863e91b3157ea50417c1362bd7bca5550
Alex Bondarenko <albom2004q@gmail.com>
2023-05-08 16:53:25 +0300

    Deleting Order prop from LogEntry.cs Model

--- Models/LogEntry.cs
+++ Models/LogEntry.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
@@ -7,15 +8,17 @@ namespace Models;
 
 public class LogEntry
 {
-    public uint Order { get; set; } = 0;
-    
-    [BsonId, BsonRepresentation(BsonType.ObjectId), Range(0, 50)]
+    [BsonId, BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    public double? GlucoseLevel { get; set; }
-    [Range(0, 50)] public double? CarbsInBreadUnits { get; set; }
-    public byte? ShortTermInsulin { get; set; }
-    public byte? LongTermInsulin { get; set; }
+    public double GlucoseLevel { get; set; }
+    [Range(0, 50)] public double CarbsInBreadUnits { get; set; }
+    
+    [DisplayName("Stop")]
+    public byte ShortTermInsulin { get; set; }
+    public byte LongTermInsulin { get; set; }
+
+    public string MeasurementPeriod { get; set; }
 
     public DateTime Date { get; set; } = DateTime.Today;
     public DateTime Time { get; set; } = DateTime.Now;
