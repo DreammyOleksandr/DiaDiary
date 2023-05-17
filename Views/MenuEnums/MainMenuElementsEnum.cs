545649b81c1903a43679e090f408387d8b1f5c8a
Alex Bondarenko <albom2004q@gmail.com>
2023-05-18 00:28:43 +0300

    Adding Enums Folder with main menu enums

---
+++ Views/MenuEnums/MainMenuElementsEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace View
+{
+	public enum MainMenuElementsEnum : byte
+	{
+		Create = 0,
+		Read = 1,
+		Update = 2,
+		Delete = 3,
+		Additional = 4,
+		About = 5,
+		Exit = 6,
+	}
+}
+
