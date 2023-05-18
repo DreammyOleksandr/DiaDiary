394dcee60307d9c5974b3973a815c37ae7b7a054
Alex Bondarenko <albom2004q@gmail.com>
2023-05-18 13:44:47 +0300

    ApplicationUserView.cs file was created

---
+++ Views/Views/ApplicationUserView.cs
@@ -0,0 +1,23 @@
+using DataAccess.Models;
+using static System.Console;
+
+namespace Views;
+
+public class ApplicationUserView
+{
+    public static ApplicationUser Register(ApplicationUser applicationUser)
+    {
+        Clear();
+        Write($"Em@il: ");
+        applicationUser.Email = ReadLine();
+        Write($"Password: ");
+        applicationUser.Password = ReadLine();
+        Write($"Full name: ");
+        applicationUser.FullName = ReadLine();
+
+        Write("\nPress any key to continue...");
+        ReadKey();
+        
+        return applicationUser;
+    }
+}
