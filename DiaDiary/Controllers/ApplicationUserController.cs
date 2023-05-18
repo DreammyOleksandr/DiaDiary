e2e95543f8dbe9bb4a047d6d67c7cbb4b3e07440
Alex Bondarenko <albom2004q@gmail.com>
2023-05-18 14:54:53 +0300

    Started working with register logic

--- DiaDiary/Controllers/ApplicationUserController.cs
+++ DiaDiary/Controllers/ApplicationUserController.cs
@@ -2,6 +2,7 @@ using System.Security.Cryptography;
 using System.Text;
 using DataAccess;
 using DataAccess.Models;
+using Views;
 
 namespace DiaDiary.Controllers;
 
@@ -11,10 +12,27 @@ public class ApplicationUserController
 
     public ApplicationUserController(string database, string applicationUserCollection)
     {
-        MongoRepository = new MongoRepository<ApplicationUser>(databaseName: database, collectionName: applicationUserCollection);
+        MongoRepository =
+            new MongoRepository<ApplicationUser>(databaseName: database, collectionName: applicationUserCollection);
     }
-    
-    string PasswordHash(string password)
+
+    public async Task Login()
+    {
+        
+    }
+
+    public async Task Register()
+    {
+        ApplicationUser applicationUser = new ApplicationUser();
+        ApplicationUserView.Register(applicationUser);
+
+        string hashedPassword = PasswordHash(applicationUser.Password);
+        applicationUser.Password = hashedPassword;
+        
+        await MongoRepository.Create(applicationUser);
+    }
+
+    private string PasswordHash(string password)
     {
         SHA256 hash = SHA256.Create();
         var passwordInBytes = Encoding.Default.GetBytes(password);
