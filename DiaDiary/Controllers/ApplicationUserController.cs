8a9a55b8184dfe329ff9063b390c76c8a251d730
Alex Bondarenko <albom2004q@gmail.com>
2023-05-18 13:34:09 +0300

    Password hashing method was created

--- DiaDiary/Controllers/ApplicationUserController.cs
+++ DiaDiary/Controllers/ApplicationUserController.cs
@@ -1,6 +1,25 @@
+using System.Security.Cryptography;
+using System.Text;
+using DataAccess;
+using DataAccess.Models;
+
 namespace DiaDiary.Controllers;
 
 public class ApplicationUserController
 {
+    private readonly MongoRepository<ApplicationUser> MongoRepository;
+
+    public ApplicationUserController(string database, string applicationUserCollection)
+    {
+        MongoRepository = new MongoRepository<ApplicationUser>(databaseName: database, collectionName: applicationUserCollection);
+    }
     
+    string PasswordHash(string password)
+    {
+        SHA256 hash = SHA256.Create();
+        var passwordInBytes = Encoding.Default.GetBytes(password);
+        var hashedPassword = hash.ComputeHash(passwordInBytes);
+
+        return Convert.ToString(hashedPassword);
+    }
 }
