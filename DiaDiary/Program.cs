bc813200195b8a28fc91d195531866d4936d5d55
Alex Bondarenko <albom2004q@gmail.com>
2023-05-21 13:14:49 +0300

    Working with Program.cs

--- DiaDiary/Program.cs
+++ DiaDiary/Program.cs
@@ -1,52 +1,68 @@
 ﻿using DiaDiary.Controllers;
-using MongoDB.Driver;
 using View;
 using View.Enums;
 
 string dbName = "DiaDiary";
-string userLogsCollection = "UserLogs";
-MongoClient client = new MongoClient();
+string userLogsCollection = "ApplicationUsers";
 
 
-
-
-
-UserLogController logEntryController = new UserLogController(dbName, userLogsCollection);
+ApplicationUserController applicationUserController = new ApplicationUserController(dbName, userLogsCollection);
 
 MenuElements menuElements = new MenuElements()
 {
     options = new string[]
     {
-        "Create", "Read", "Update", "Delete", "About", "Exit",
+        "Login", "Registration", "Exit"
     }
 };
-
 
 while (true)
 {
     ScrollableMenu scrollableMenu = new ScrollableMenu(menuElements);
     int chosenAction = scrollableMenu.Run();
 
-    switch ((MainMenuElement)chosenAction)
+    switch ((AuthorizationEnum)chosenAction)
     {
-        case MainMenuElement.Create:
-            await logEntryController.Create();
+        case AuthorizationEnum.Login:
+            await applicationUserController.Login();
             break;
-        case MainMenuElement.Read:
-            logEntryController.GetAll();
+        case AuthorizationEnum.Registration:
+            await applicationUserController.Register();
             break;
-        case MainMenuElement.Update:
-            await logEntryController.Update();
-            break;
-        case MainMenuElement.Delete:
-            await logEntryController.Delete();
-            break;
-        case MainMenuElement.About:
-            Console.Clear();
-            ContextActions.About();
-            break;
-        case MainMenuElement.Exit:
+        case AuthorizationEnum.Exit:
             ContextActions.Exit();
             break;
     }
 }
+
+
+//
+//
+// while (true)
+// {
+//     ScrollableMenu scrollableMenu = new ScrollableMenu(menuElements);
+//     int chosenAction = scrollableMenu.Run();
+//
+//     switch ((MainMenuElement)chosenAction)
+//     {
+//         case MainMenuElement.Create:
+//             await logEntryController.Create();
+//             break;
+//         case MainMenuElement.Read:
+//             logEntryController.GetAll();
+//             break;
+//         case MainMenuElement.Update:
+//             await logEntryController.Update();
+//             break;
+//         case MainMenuElement.Delete:
+//             await logEntryController.Delete();
+//             break;
+//         case MainMenuElement.About:
+//             Console.Clear();
+//             ContextActions.About();
+//             break;
+//         case MainMenuElement.Exit:
+//             ContextActions.Exit();
+//             break;
+//     }
+// }
