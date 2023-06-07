f7c8c75949e6cfb6c99cb0c3a4242c69daf50a94
Alex Bondarenko <albom2004q@gmail.com>
2023-06-07 11:17:57 +0300

    Ended working with Startup.cs

--- DiaDiary/Startup.cs
+++ DiaDiary/Startup.cs
@@ -2,27 +2,19 @@
 using DataAccess.Models;
 using DiaDiary;
 using DiaDiary.Controllers;
-using View;
 
 MongoRepository<ApplicationUser> mongoRepositoryForAppUser =
     new MongoRepository<ApplicationUser>(DbStrings.DbName, DbStrings.ApplicationUsersCollection);
 ApplicationUser applicationUser = new();
 
-ApplicationUserController applicationUserController = new ApplicationUserController(mongoRepositoryForAppUser, applicationUser);
+ApplicationUserController applicationUserController =
+    new ApplicationUserController(mongoRepositoryForAppUser, applicationUser);
 
-while (applicationUser.Email == null)
-{
-    ScrollableMenu scrollableMenu = new ScrollableMenu(applicationUserController.menuElements);
-    int chosenAction = scrollableMenu.Run();
-    dynamic execution = applicationUserController.ActionsRouting(chosenAction);
-}
+MongoRepository<UserLog> mongoRepositoryForUserLogs =
+    new MongoRepository<UserLog>(DbStrings.DbName, DbStrings.UserLogsCollection);
 
-MongoRepository<UserLog> mongoRepository = new MongoRepository<UserLog>(DbStrings.DbName, DbStrings.UserLogsCollection);
-UserLogController userLogController = new(mongoRepository, applicationUser);
+UserLogController userLogController =
+    new(mongoRepositoryForUserLogs, mongoRepositoryForAppUser, applicationUser);
 
-while (true)
-{
-    ScrollableMenu scrollableMenu = new ScrollableMenu(userLogController.menuElements);
-    int chosenAction = scrollableMenu.Run();
-    dynamic execution = userLogController.ActionsRouting(chosenAction);
-}
+applicationUserController.AuthenticationActions(applicationUser);
+userLogController.UserActions();
